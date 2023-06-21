5bce8fe5326aad4b065c06648f2b7385a9ba1bba
LukasdeSouza <lucas@bestdeal.com.br>
2023-06-21 11:09:55 -0300

    fix: correção de rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import HomePage from "./pages/home";
 import RootLayout from "./components/RootLayout";
 import './App.css'
 import ProfilePage from "./pages/profile";
 import SettingsPage from "./pages/settings";
-import ResidentsPage from "./pages/commerce";
 import InvoicesPage from "./pages/invoices";
 import AnnouncementsPage from "./pages/announcements";
 import CommercePage from "./pages/commerce";
 import DashboardPage from "./pages/dashboard";
 import ProductsPage from "./pages/products";
 import EmployeesPage from "./pages/employees";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
         {/* <Route path="/login" element={<LoginPage />} /> */}
         <Route path="/perfil" element={<ProfilePage />} />
         <Route path="/configuracoes" element={<SettingsPage />} />
