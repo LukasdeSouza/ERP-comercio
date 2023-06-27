73bb43bd38132fbb14445f50067e9505fc6b8799
LukasdeSouza <lucas@bestdeal.com.br>
2023-06-26 22:08:39 -0300

    feature: adicionando gráfico a tela de informações financeiras

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,6 +3,7 @@ import MainPage from '../main'
 import { Typography } from '@mui/material'
 import BasicTable from '../../components/BasicTable'
 import { useNavigate } from 'react-router-dom'
+import BarChart from '../../components/Charts/barChart'
 
 const FinancialPage = () => {
   const navigate = useNavigate()
@@ -12,8 +13,10 @@ const FinancialPage = () => {
 
   return (
     <MainPage>
-      <Typography fontFamily={'Poppins'} fontSize={18} mb={2}>Lista de Informações Financeiras</Typography>
-      <BasicTable onClickEdit={onClickEdit} />
+      <Typography fontFamily={'Poppins'} fontSize={18} mb={2}>Informações Financeiras</Typography>
+      <Typography fontFamily={'Poppins'} fontSize={14} mb={2}>Entradas e Saídas de Valores</Typography>
+      {/* <BasicTable onClickEdit={onClickEdit} /> */}
+      <BarChart />
     </MainPage>
   )
 }
