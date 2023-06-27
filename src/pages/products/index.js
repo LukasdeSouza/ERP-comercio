5f4a1d7ce186c5ec35e65bac9ffa172504ac3a33
LukasdeSouza <lucas@bestdeal.com.br>
2023-06-27 20:36:13 -0300

    feature: tentiva de row na página de products

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -6,6 +6,12 @@ import { useNavigate } from 'react-router-dom'
 
 const ProductsPage = () => {
   const navigate = useNavigate()
+  const tableHead = ['Código', 'Produto', 'Estoque', 'Valor Unitário', 'Valor Total', 'Ações']
+  const tableRow = [
+    { code: '42343', product: 'Cenoura', quantity: 34, unityValue: 'R$0,55', value: 'R$64.93' },
+    { code: '12314', product: 'Requeijão', quantity: 22, unityValue: 'R$4,90', value: 'R$88.93' },
+    { code: '52342', product: 'Carne', quantity: 12, unityValue: 'R$32,48', value: 'R$96.93' }
+  ]
 
   const onClickEdit = (row) => {
     navigate(`/produtos/${row.code}`)
@@ -16,7 +22,10 @@ const ProductsPage = () => {
       <Typography fontFamily={'Poppins'} fontSize={18} mb={2}>
         Lista de Produtos
       </Typography>
-      <BasicTable onClickEdit={onClickEdit} />
+      <BasicTable
+        tableHead={tableHead}
+        tableRow={tableRow}
+        onClickEdit={onClickEdit} />
     </MainPage>
   )
 }
